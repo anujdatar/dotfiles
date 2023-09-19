b30e2348fd3a9a38db6c610f2a8e8807e86493de
Anuj Datar <anuj.datar@gmail.com>
2023-09-20 03:17:17 +0530

    Added vimwiki and taskwiki plugins

---
+++ nvim/lua/plugins/taskwiki.lua
@@ -0,0 +1,5 @@
+return {
+    {
+        'https://github.com/tools-life/taskwiki',
+    }
+}
---
+++ nvim/lua/plugins/vimwiki.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        'vimwiki/vimwiki',
+        init = function()
+            vim.g.vimwiki_global_ext = 0
+            vim.g.vimwiki_list = {{
+                path = '/media/anuj/Data/Notes',
+                syntax = 'markdown',
+                ext = '.md',
+            }}
+        end,
+    }
+}
