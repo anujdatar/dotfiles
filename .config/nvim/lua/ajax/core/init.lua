8b6ea1311a1c413c73b4ae89c11c9f2d466735c5
Anuj Datar <anuj.datar@gmail.com>
2024-04-25 00:26:20 +0530

    not using the utils file atm, removed dead-code-comments

--- .config/nvim/lua/ajax/core/init.lua
+++ .config/nvim/lua/ajax/core/init.lua
@@ -1,8 +1,3 @@
 require('ajax.core.settings')
 require('ajax.core.mappings')
-require('ajax.core.utils')
-
--- setup keymap at the end
--- local utils = require('ajax.core.utils')
--- local mappings = require('mappings')
--- utils.set_keymaps(mappings)
+-- require('ajax.core.utils')
