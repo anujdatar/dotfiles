afa7e251950ac6d00be68db977002defdd84923b
Anuj Datar <anuj.datar@gmail.com>
2024-04-25 12:00:55 +0530

    Fix: moved the format command into the lsp on-attach section, for buf access

--- .config/nvim/lua/ajax/plugins/lsp/lsp-config.lua
+++ .config/nvim/lua/ajax/plugins/lsp/lsp-config.lua
@@ -73,13 +73,14 @@ return{
 
                 opts.desc = '[R]e[S]tart LSP'
                 vim.keymap.set('n', '<leader>rs', ':LspRestart<CR>', opts)
+
+                -- create a command `:Format` local to the LSP buffer
+                vim.api.nvim_buf_create_user_command(ev.buf, 'Format', function(_)
+                    vim.lsp.buf.format()
+                    end, {desc = 'Format current buffer with LSP'}
+                )
             end,
 
-            -- create a command `:Format` local to the LSP buffer
-            vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-                vim.lsp.buf.format()
-                end, {desc = 'Format current buffer with LSP'}
-            )
         })
 
         -- enable autocompletion (assign to every lsp server)
