3c20c329d6935deae2650bd8892105eab3cc8753
Anuj Datar <anuj.datar@gmail.com>
2024-04-30 18:43:26 +0530

    Added the nerd fonts missing fix, from kickstart

--- .config/nvim/lua/ajax/lazy.lua
+++ .config/nvim/lua/ajax/lazy.lua
@@ -18,12 +18,31 @@ require('lazy').setup({
     { import = 'ajax.plugins' },
     { import = 'ajax.plugins.lsp' },
 }, {
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		enabled = true,
-		notify = false,
-	},
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    change_detection = {
+        enabled = true,
+        notify = false,
+    },
+    ui = {
+        -- If you are using a Nerd Font: set icons to an empty table which will use the
+        -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+        icons = vim.g.have_nerd_font and {} or {
+            cmd = '⌘',
+            config = '🛠',
+            event = '📅',
+            ft = '📂',
+            init = '⚙',
+            keys = '🗝',
+            plugin = '🔌',
+            runtime = '💻',
+            require = '🌙',
+            source = '📄',
+            start = '🚀',
+            task = '📌',
+            lazy = '💤 ',
+        },
+    },
 })
