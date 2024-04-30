fbdfd2e3ba7a34ec861f7be30a0b83af088cc5cf
Anuj Datar <anuj.datar@gmail.com>
2024-04-30 18:50:52 +0530

    Fix: Redid the while lsp and mason config, lsp wasn't correctly working
    None of the `on_attach` stuff was being loaded. Used kickstart template

--- .config/nvim/lua/ajax/plugins/lsp/lsp-config.lua
+++ .config/nvim/lua/ajax/plugins/lsp/lsp-config.lua
@@ -1,88 +1,307 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		{ "antosha417/nvim-lsp-file-operations", config = true }, -- imports on file rename
-		{ "folke/neodev.nvim", opts = {} }, -- better lsp for lua
-		{ "j-hui/fidget.nvim", tag = "legacy", opts = {} }, -- lsp status updates
-		{ "williamboman/mason.nvim" }, -- mason
-		{ "williamboman/mason-lspconfig.nvim" }, -- mason lsp
-	},
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        { 'williamboman/mason.nvim', config = true }, -- must be loaded before i think
+        { 'williamboman/mason-lspconfig.nvim' },
+        { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
+        { 'j-hui/fidget.nvim', opts = {} }, -- useful for status updates updates
+        { 'folke/neodev.nvim', opts = {} }, -- better lsp for lua
+    },
+    config = function()
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup(
+                'UserLspAttach',
+                { clear = true }
+            ),
+            callback = function(event)
+                local map = function(mode, keys, func, desc)
+                    vim.keymap.set(
+                        mode,
+                        keys,
+                        func,
+                        { buffer = event.buf, desc = desc }
+                    )
+                end
 
-	config = function()
-		local lspconfig = require("lspconfig")
+                local t_builtin = require('telescope.builtin')
 
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-			callback = function(ev)
-				local opts = { buffer = ev.buf, silent = true }
+                -- jump to var or function definition/declaration
+                map(
+                    'n',
+                    '<leader>gd',
+                    t_builtin.lsp_definitions,
+                    '[G]o to [D]efinition'
+                )
 
-				otps.desc = "[R]e[N]ame"
-				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                -- find references of work under cursor
+                map(
+                    'n',
+                    '<leader>gr',
+                    t_builtin.lsp_references,
+                    '[G]o to [R]eferences'
+                )
 
-				opts.desc = "[C]ode [A]ction"
-				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+                -- jump to implementation of interface
+                -- where declaration and implementation are different
+                map(
+                    'n',
+                    '<leader>gi',
+                    t_builtin.lsp_implementations,
+                    '[G]o to [I]mplementation'
+                )
 
-				opts.desc = "[G]]o to [D]efinition"
-				vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+                -- jump to type definition
+                map(
+                    'n',
+                    '<leader>gt',
+                    t_builtin.lsp_type_definitions,
+                    '[G]o to [T]ype definition'
+                )
 
-				opts.desc = "[G]o to [R]]eferences"
-				vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+                -- fuzzy find all symbols (vars, funcs, types) in document
+                map(
+                    'n',
+                    '<leader>ds',
+                    t_builtin.lsp_document_symbols,
+                    'Go to [D]ocument [S]ymbol'
+                )
 
-				opts.desc = "[G]o to [I]mplementation"
-				vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, opts)
+                -- fuzzy find all symbols (vars, funcs, types) in workspace/project
+                map(
+                    'n',
+                    '<leader>ws',
+                    t_builtin.lsp_workspace_symbols,
+                    'Go to [W]orkspace [S]ymbol'
+                )
 
-				opts.desc = "[G]o to [T]ype definition"
-				vim.keymap.set("n", "<leader>gt", vim.lsp.buf.type_definition, opts)
+                -- Rename the variable under your cursor, should work across files
+                map('n', '<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 
-				opts.desc = "[G]o to [D]eclaration" -- not used often
-				vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts)
+                -- Execute a code action, usually your cursor needs to be on top of an error
+                -- or a suggestion from your LSP for this to activate.
+                map(
+                    'n',
+                    '<leader>ca',
+                    vim.lsp.buf.code_action,
+                    '[C]ode [A]ction'
+                )
 
-				opts.desc = "Go to [D]ocument [S]ymbol"
-				vim.keymap.set("n", "<leader>ds", vim.lsp.buf.document_symbol, opts)
+                -- displays docs about the word under your cursor
+                --  See `:help K` for why this keymap.
+                map('n', 'K', vim.lsp.buf.hover, 'Hover Documentation')
 
-				opts.desc = "Go to [W]orkspace [S]ymbol"
-				vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
+                -- Displays the signature of the function under your cursor
+                map('n', '<C-k>', vim.lsp.buf.signature_help, 'Signature Help')
 
-				opts.desc = "Hover Documentation" -- shows documentation of hovered item
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                -- WARN: This is not Goto Definition, this is Goto Declaration.
+                --  For example, in C this would take you to the header.
+                map(
+                    'n',
+                    '<leader>gD',
+                    vim.lsp.buf.declaration,
+                    '[G]oto [D]eclaration'
+                )
 
-				opts.desc = "Signature Documentation" -- shows signature of hovered item
-				vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+                map(
+                    'n',
+                    '<leader>wa',
+                    vim.lsp.buf.add_workspace_folder,
+                    '[W]orkspace: [A]dd Folder'
+                )
 
-				opts.desc = "[W]orkspace [A]dd folder"
-				vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+                map(
+                    'n',
+                    '<leader>wr',
+                    vim.lsp.buf.remove_workspace_folder,
+                    '[W]orkspace: [R]emove Folder'
+                )
 
-				opts.desc = "[W]orkspace [R]emove folder"
-				vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+                -- map('n', '<leader>wn', vim.lsp.buf.rename_workspace_folder, '[W]orkspace: re[N]ame Folder')
 
-				opts.desc = "[W]orkspace re[N]ame folder"
-				vim.keymap.set("n", "<leader>wn", vim.lsp.buf.rename_workspace_folder, opts)
+                map('n', '<leader>wl', function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, '[W]orkspace: [L]ist Folders')
 
-				opts.desc = "[W]orkspace [L]ist folders"
-				vim.keymap.set("n", "<leader>wl", function()
-					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-				end, opts)
+                map(
+                    'n',
+                    '<leader>ef',
+                    '<cmd>EslintFixAll<CR>',
+                    '[E]slint [F]ix All'
+                )
 
-				opts.desc = "[E]slint [F]ix Formatting"
-				vim.keymap.set("n", "<leader>ef", "<cmd>EslintFixAll<CR>", opts)
+                map('n', '<leader>rs', ':LspRestart<CR>', '[R]e[S]tart LSP')
 
-				opts.desc = "[R]e[S]tart LSP"
-				vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+                -- create a command `:Format` local to the LSP buffer
+                vim.api.nvim_buf_create_user_command(
+                    event.buf,
+                    'Format',
+                    function(_)
+                        vim.lsp.buf.format()
+                    end,
+                    { desc = 'Format current buffer with LSP' }
+                )
 
-				-- create a command `:Format` local to the LSP buffer
-				vim.api.nvim_buf_create_user_command(ev.buf, "Format", function(_)
-					vim.lsp.buf.format()
-				end, { desc = "Format current buffer with LSP" })
-			end,
-		})
+                -- autocommands for highlighting references of words under cursor
+                -- after the cursor rests for a while
+                --      See `:help CursorHold` for info
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
-	end,
+                -- highlights will be cleared when cursor is moved
+                local client = vim.lsp.get_client_by_id(event.data.client_id)
+                if
+                    client
+                    and client.server_capabilities.documentHighlightProvider
+                then
+                    local highlight_augroup = vim.api.nvim_create_augroup(
+                        'UserLspHighlight',
+                        { clear = false }
+                    )
+                    vim.api.nvim_create_autocmd(
+                        { 'CursorHold', 'CursorHoldI' },
+                        {
+                            buffer = event.buf,
+                            group = highlight_augroup,
+                            callback = vim.lsp.buf.document_highlight,
+                        }
+                    )
+
+                    vim.api.nvim_create_autocmd(
+                        { 'CursorMoved', 'CursorMovedI' },
+                        {
+                            buffer = event.buf,
+                            group = highlight_augroup,
+                            callback = vim.lsp.buf.clear_references,
+                        }
+                    )
+                end
+
+                -- autocmd to enable inlay hints
+                -- may have undesired effects, might displace some code
+                if
+                    client
+                    and client.server_capabilities.inlayHintProvider
+                    and vim.lsp.inlay_hint
+                then
+                    map('<leader>ht', function()
+                        vim.lsp.inlay_hit.enable(
+                            not vim.lsp.inlay_hint.is_enabled()
+                        )
+                    end, '[H]ints [T]oggle')
+                end
+            end,
+        })
+
+        vim.api.nvim_create_autocmd('LspDetach', {
+            group = vim.api.nvim_create_augroup(
+                'UserLspDetach',
+                { clear = true }
+            ),
+            callback = function(event)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds({
+                    group = 'UserLspHighlight',
+                    buffer = event.buf,
+                })
+            end,
+        })
+
+        -- Change the Diagnostic symbols in the sign column (gutter)
+        local signs =
+            { Error = ' ', Warn = ' ', Hint = '󰠠 ', Info = ' ' }
+        for type, icon in pairs(signs) do
+            local hl = 'DiagnosticSign' .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+        end
+
+        -- adding cmp capabilities to lsp
+        local cmp_nvim_lsp = require('cmp_nvim_lsp')
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = vim.tbl_deep_extend(
+            'force',
+            capabilities,
+            cmp_nvim_lsp.default_capabilities()
+        )
+
+        -- setup mason and ensure all the default language servers are installed
+        -- define custom server options, not necessary for most, using kickstart template
+        local servers = {
+            lua_ls = {
+                -- cmd = {...},
+                -- filetypes = {...},
+                -- capabilities = {...},
+                settings = {
+                    Lua = {
+                        completion = {
+                            callSnippet = 'Replave',
+                        },
+                        -- to ignore Lua_LS's noisy `missing-fields` warnings
+                        diagnostics = {
+                            disable = { 'missing-fields' },
+                        },
+                    },
+                },
+            },
+            bashls = {},
+            clangd = {},
+            dockerls = {},
+            eslint = {},
+            gopls = {},
+            ltex = {},
+            pyright = {},
+            rust_analyzer = {},
+            jsonls = {},
+            yamlls = {},
+            tsserver = {},
+            astro = {},
+            svelte = {},
+            volar = {},
+            cssls = {},
+            tailwindcss = {},
+            html = {},
+            htmx = {},
+        }
+
+        local ensure_installed = vim.tbl_keys(servers or {})
+        vim.list_extend(ensure_installed, {
+            'stylua',
+            'clang-format',
+            'eslint_d',
+            'prettier',
+            'pylint',
+            'isort',
+            'black',
+            'goimports',
+            'golangci-lint',
+            'shellcheck',
+        })
+
+        require('mason').setup({
+            ui = {
+                icons = {
+                    package_pending = ' ',
+                    package_installed = '󰄳 ',
+                    package_uninstalled = ' 󰚌',
+                },
+            },
+        })
+
+        require('mason-tool-installer').setup({
+            ensure_installed = ensure_installed,
+        })
+
+        require('mason-lspconfig').setup({
+            handlers = {
+                function(server_name)
+                    local server = servers[server_name] or {}
+
+                    server.capabilities = vim.tbl_deep_extend(
+                        'force',
+                        {},
+                        capabilities,
+                        server.capabilities or {}
+                    )
+                    require('lspconfig')[server_name].setup(server)
+                end,
+            },
+        })
+    end,
 }
