a6d99c2dae205a4e15c704c034dadfd24ac309ee
Anuj Datar <anuj.datar@gmail.com>
2024-05-20 17:26:21 +0530

    Fix: Comment.nvim keymaps work properly in vis-mode, added block comments

--- .config/nvim/lua/ajax/plugins/comment.lua
+++ .config/nvim/lua/ajax/plugins/comment.lua
@@ -7,27 +7,51 @@ return {
     },
     config = function()
         local comment = require('Comment')
+        local api = require('Comment.api')
 
         local ts_context_commentstring =
             require('ts_context_commentstring.integrations.comment_nvim')
 
-        -- keymaps
+        -- keymaps - based on Comment.nvim documentation
+        -- for some reason <C-_> here works as C-/
         vim.keymap.set(
             'n',
-            '<leader>/',
-            require('Comment.api').toggle.linewise.current,
+            '<C-_>',
+            api.toggle.linewise.current,
             { desc = 'Toggle comment on current line' }
         )
         vim.keymap.set(
-            'v',
-            '<leader>/',
-            require('Comment.api').toggle.linewise.current,
-            { desc = 'Toggle comment on current line' }
+            'n',
+            '<C-\\>',
+            api.toggle.blockwise.current,
+            { desc = 'Toggle comment on selected line' }
         )
+
+        -- enable toggling selected text
+        local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
+
+        -- for some reason <C-_> here works as C-/
+        vim.keymap.set('x', '<C-_>', function()
+            vim.api.nvim_feedkeys(esc, 'nx', false)
+            api.toggle.linewise(vim.fn.visualmode())
+        end, { desc = 'Toggle comment on current line' })
+        vim.keymap.set('x', '<C-\\>', function()
+            vim.api.nvim_feedkeys(esc, 'nx', false)
+            api.toggle.blockwise(vim.fn.visualmode())
+        end, { desc = 'Toggle comment on selected line' })
 
         comment.setup({
             -- for commenting tsx and jsx files
             pre_hook = ts_context_commentstring.create_pre_hook(),
+            -- replace the regular 'gc' and 'gb' keymaps, use above to keep original also
+            -- toggler = {
+            --     line = '<leader>/',
+            --     block = '<leader>\\',
+            -- },
+            -- opleader = {
+            --     line = '<leader>/',
+            --     block = '<leader>\\',
+            -- },
         })
     end,
 }
