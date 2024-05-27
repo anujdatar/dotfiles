c50541528633cc758e59447126aea6dcb861a198
Anuj Datar <anuj.datar@gmail.com>
2024-05-27 19:01:36 +0530

    Changed active nvim theme from tokyonight to nightfly

--- .config/nvim/lua/ajax/plugins/colorscheme.lua
+++ .config/nvim/lua/ajax/plugins/colorscheme.lua
@@ -6,13 +6,13 @@ return {
     -- 		vim.cmd.colorscheme('onedark')
     -- 	end,
     -- },
-    {
-        'folke/tokyonight.nvim',
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme('tokyonight')
-        end,
-    },
+    -- {
+    -- 'folke/tokyonight.nvim',
+    -- priority = 1000,
+    -- config = function()
+    -- vim.cmd.colorscheme('tokyonight')
+    -- end,
+    -- },
     -- {
     -- 	'rebelot/kanagawa.nvim',
     -- 	priority = 1000,
@@ -20,11 +20,11 @@ return {
     -- 		vim.cmd.colorscheme('kanagawa')
     -- 	end,
     -- },
-    -- {
-    -- 	'bluz71/vim-nightfly-guicolors',
-    -- 	priority = 1000,
-    -- 	config = function()
-    -- 		vim.cmd.colorscheme('nightfly')
-    -- 	end,
-    -- },
+    {
+        'bluz71/vim-nightfly-guicolors',
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme('nightfly')
+        end,
+    },
 }
